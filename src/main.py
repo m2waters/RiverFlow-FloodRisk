6d2a693cbe9a9754515674c256db6f31edc3c3df
m2waters <mwaters2110@gmail.com>
2025-09-07 11:35:28 +0100

    pulled in the River flow data from NRFA API to explore the dataset as well as locating a rainfall dataset from the MET OFFICE, rainful dataset is not time series though so will have to look elsewhere

--- src/main.py
+++ src/main.py
@@ -1,1 +1,38 @@
-print("hello world")
+import requests as re
+import pandas as pd
+import json
+
+
+request = re.get("https://nrfaapps.ceh.ac.uk/nrfa/ws/station-info?station=*&format=json-object&fields=all")
+# print(request)
+
+j = json.loads(request.text)
+
+df = pd.DataFrame(j["data"])
+
+print("River Flow Data:")
+print("###############################################################")
+print(df.head())
+print(df.columns)
+print(df.describe())
+print("###############################################################")
+
+rainfallRequest = re.get("https://services.arcgis.com/Lq3V5RFuTBC9I7kv/arcgis/rest/services/Monthly_Precipitation_Observations_1991_2020_12km/FeatureServer/1/query?outFields=*&where=1%3D1&f=geojson")
+
+geoJson = json.loads(rainfallRequest.text)
+
+rainfallDf = pd.DataFrame(geoJson["features"])
+
+print("Uk Monthly Rainfall Data:")
+print("###############################################################")
+print(rainfallDf.head())
+print(rainfallDf.columns)
+print(rainfallDf.describe())
+firstRowGeometry = pd.DataFrame(rainfallDf.iloc[0]["geometry"])
+print(firstRowGeometry.head())
+print(firstRowGeometry.columns)
+print(rainfallDf.iloc[0]["properties"])
+# firstRowProperties = pd.DataFrame(rainfallDf.iloc[0]["properties"])
+# print(firstRowProperties.head())
+# print(firstRowProperties.columns)
+print("###############################################################")
